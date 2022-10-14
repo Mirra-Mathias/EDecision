dabf626c845dbd4ffd87c07ff544d361545955af
Mirra Mathias <52259162+Mirra-Mathias@users.noreply.github.com>
2022-10-14 11:48:57 +0200

    init

---
+++ src/test/java/fr/ipi/edecision/EDecisionApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ipi.edecision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDecisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
