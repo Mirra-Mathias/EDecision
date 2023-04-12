3f48f7876998e84fbdf7bebbe61e92ae6bcc539a
mirra-mathias <mathiasmirra@gmail.com>
2023-04-12 22:38:56 +0200

    add auth, equipe, projets, propo, votes

--- src/main/java/fr/ipi/edecision/repositories/MembreRepositorie.java
+++
@@ -1,9 +0,0 @@
-package fr.ipi.edecision.repositories;
-
-import fr.ipi.edecision.entities.Membre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MembreRepositorie extends JpaRepository<Membre, Integer> {
-}
